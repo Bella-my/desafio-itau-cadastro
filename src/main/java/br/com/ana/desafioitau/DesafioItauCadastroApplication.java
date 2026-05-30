3baaf7709dfa05adc9bd5bdceee40092f95cf125
Ana Vitoria <annabela.oliver@gmail.com>
2026-05-30 18:48:05 -0300

    chore: desafio aceito e ambiente configurado

---
+++ src/main/java/br/com/ana/desafioitau/DesafioItauCadastroApplication.java
@@ -0,0 +1,13 @@
+package br.com.ana.desafioitau;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioItauCadastroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesafioItauCadastroApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/ana/desafioitau/DesafioItauCadastroApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ana.desafioitau;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioItauCadastroApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
