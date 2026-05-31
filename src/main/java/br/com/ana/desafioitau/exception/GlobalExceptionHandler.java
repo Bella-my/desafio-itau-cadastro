b51adf900804226f434944131d4f05f1becec0d0
Ana Vitoria <annabela.oliver@gmail.com>
2026-05-31 10:42:26 -0300

    feat: add Tratamento de erros

---
+++ src/main/java/br/com/ana/desafioitau/exception/GlobalExceptionHandler.java
@@ -0,0 +1,24 @@
+package br.com.ana.desafioitau.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> tratarErrosDeValidacao(MethodArgumentNotValidException exception) {
+        Map<String, String> erros = new HashMap<>();
+
+        exception.getBindingResult().getFieldErrors().forEach(erro -> {
+            erros.put(erro.getField(), erro.getDefaultMessage());
+        });
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(erros);
+    }
+}
