5cb6841531d0f92c4671b58d8c97dbddf69448c8
Ana Vitoria <annabela.oliver@gmail.com>
2026-06-02 21:29:57 -0300

    chore: permite nova origem local do front

--- src/main/java/br/com/ana/desafioitau/controller/PersonController.java
+++ src/main/java/br/com/ana/desafioitau/controller/PersonController.java
@@ -12,7 +12,7 @@ import br.com.ana.desafioitau.dto.PersonResponseDTO;
 import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = {"http://localhost:5173", "http://localhost:5174"})
 @RestController
 @RequestMapping("/pessoas")
 public class PersonController {
